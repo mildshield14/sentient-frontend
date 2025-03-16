445f8158d10a8221fb0f9252abcf41298486b165
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-03-16 13:47:00 -0400

    add time and date

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,6 +1,8 @@
 import  React, { useState , useEffect } from 'react'
 import "../scss/Home.scss"
 
+// inspired from https://medium.com/create-a-clocking-in-system-on-react/create-a-react-app-displaying-the-current-date-and-time-using-hooks-21d946971556
+
 export const DateTime = () => {
 
   const [date,setDate] = useState(new Date());
